2337807b151203e572e59c518dd70dbad3206e6d
Trần Vũ <124017908+tranvuxdev@users.noreply.github.com>
2026-04-13 02:48:48 +0700

    light house mobile optimize

--- components/user/motion-provider.tsx
+++ components/user/motion-provider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { LazyMotion, domAnimation } from "motion/react";
+import { LazyMotion, domAnimation } from "framer-motion";
 import React from "react";
 
 export function MotionProvider({ children }: { children: React.ReactNode }) {
