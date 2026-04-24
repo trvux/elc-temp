b4750e54c6b98ccdf00a36804d29a7ab4ee79193
Trần Vũ <124017908+tranvuxdev@users.noreply.github.com>
2026-04-25 04:36:04 +0700

    logo

--- components/user/header.tsx
+++ components/user/header.tsx
@@ -161,6 +161,7 @@ export function Header() {
                 width={120} 
                 height={40} 
                 className="h-10 w-auto"
+                style={{ width: "auto" }}
                 priority
               />
             </Link>
