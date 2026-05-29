d1488435a2ed03d2af5fe16d2d916cbaa6d22d61
Trần Vũ <124017908+tranvuxdev@users.noreply.github.com>
2026-05-29 07:52:10 +0700

    feat: add localPatterns to allow image loading from /images/** path

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,11 @@ const nextConfig: NextConfig = {
         hostname: "media.dienmayelc.com.vn",
       },
     ],
+    localPatterns: [
+      {
+        pathname: "/images/**",
+      },
+    ],
   },
 };
 
